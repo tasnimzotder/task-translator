11b39373a989c37bef04158ba11c75d7cdf48cc8
Tasnim Zotder <tasnimzotder@gmail.com>
2023-06-08 19:12:20 +0530

    add style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,30 @@
+import Footer from '@/components/Footer';
 import InputBox from '@/components/InputBox';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full text-center">
+    <main className="flex min-h-screen flex-col justify-between p-3">
+      <div className="w-full text-center flex-col items-center justify-between p-24">
         <div>
-          {/* Title */}
-          <div className="text-xl">Task Translator</div>
+          <div>
+            {/* Title */}
+            <div className="text-xl">Task Translator</div>
 
-          {/* Subtitle */}
-          <div className="text-sm text-gray-500">
-            Translate your tasks into your CLI commands
+            {/* Subtitle */}
+            <div className="text-sm text-gray-200">
+              Translate your tasks into your CLI commands
+            </div>
+          </div>
+
+          <div className="my-10">
+            {/* Input box */}
+            <InputBox />
           </div>
         </div>
-
-        <div className="my-10">
-          {/* Input box */}
-          <InputBox />
-        </div>
       </div>
+
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer className="text-center">&copy; 2021 Tasnim Zotder</footer>;
+};
+
+export default Footer;
